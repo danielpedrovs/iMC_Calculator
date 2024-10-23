15365cc61c94286eacc8a27b5cce1129a1ab5a9f
danielpedrovs <danielpedrovs@gmail.com>
2024-10-23 20:38:12 -0300

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,52 @@
+function meuEscopo(){
+
+    if (!peso || !altura) {
+        imcMessage.innerHTML = "<p>Por favor, insira valores válidos.</p>";
+        alert("Por favor, insira valores válidos.");
+        return;
+      }
+      
+      
+const form = document.querySelector('.form');
+const imcMessage = document.querySelector('#imcMessage');
+
+function recebeeventForm (evento){
+
+evento.preventDefault();
+let peso  = form.querySelector('.pesoin');
+let altura = form.querySelector('.alturain');
+
+let iMC = peso.value / (altura.value*altura.value);
+
+let im = iMC.toFixed(1);
+
+let resultado;
+
+    if(im < 18.5){ 
+    resultado = "abaixo do peso" }else 
+    if( 18.5 < im && im <= 24.9){
+    resultado = "Peso Normal"};
+    if(25 < im && im < 29.9){
+        resultado = "SobrePeso"
+    }else if(30 < im && im < 34.9){
+        resultado = "Obesidade Grau1"
+    }else if(35<im && im <39.9){
+        resultado = "Obsesidade Grau2"
+    }else if(im > 40){
+        resultado = "Obesidade Grau3"
+    }
+
+
+alert (`Seu IMC:
+ ${im}  - indica: ${resultado} `)
+    imcMessage.innerHTML = `<p><b>IMC:
+ ${im}  - ${resultado} </b></p>`;
+
+    
+}
+
+
+form.addEventListener('submit', recebeeventForm);
+}
+
+meuEscopo();
